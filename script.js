a050ee25ad47a8c032913c69997301967597f703
MajKrzysztof <krzysztofmaj94@gmail.com>
2018-01-02 22:46:54 +0100

    3 zmiana mayski

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ $(window).on("scroll", function ()
     if($("nav").offset().top > 10)
     {
         $("nav").addClass("scrolled");
-        $("nav img").attr("src","src=\"http://taureca.com/wp-content/themes/taureca/img/logo.png\"");
+        $("nav img").attr("src","logo.png");
+
     }
     else
     {
         $("nav").removeClass("scrolled");
+        $("nav img").attr("src","logo2.png");
     }
     console.log($("nav").offset());
 });
